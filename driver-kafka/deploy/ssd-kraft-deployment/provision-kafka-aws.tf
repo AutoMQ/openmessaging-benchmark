fdee89b1ba7ba430635daacf1b242829515a7486
Curtis Wan <wcy9988@163.com>
2023-05-28 17:19:15 +0800

    feat: add support for kafka benchmark in kraft mode (#5)
    Signed-off-by: Curtis Wan <wcy9988@163.com>

---
+++ driver-kafka/deploy/ssd-kraft-deployment/provision-kafka-aws.tf
@@ -0,0 +1,168 @@
+provider "aws" {
+  region  = "${var.region}"
+}
+
+provider "random" {}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/kafka_aws.pub
+DESCRIPTION
+}
+
+resource "random_id" "hash" {
+  byte_length = 8
+}
+
+variable "key_name" {
+  default     = "kafka-benchmark-key"
+  description = "Desired name prefix for the AWS key pair"
+}
+
+variable "region" {}
+
+variable "ami" {}
+
+variable "az" {}
+
+variable "instance_types" {
+  type = map(string)
+}
+
+variable "num_instances" {
+  type = map(string)
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "benchmark_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Kafka_Benchmark_VPC_${random_id.hash.hex}"
+  }
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "kafka" {
+  vpc_id = "${aws_vpc.benchmark_vpc.id}"
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.benchmark_vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.kafka.id}"
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "benchmark_subnet" {
+  vpc_id                  = "${aws_vpc.benchmark_vpc.id}"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.az}"
+}
+
+resource "aws_security_group" "benchmark_security_group" {
+  name   = "terraform-kafka-${random_id.hash.hex}"
+  vpc_id = "${aws_vpc.benchmark_vpc.id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # All ports open within the VPC
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Benchmark-Security-Group-${random_id.hash.hex}"
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}-${random_id.hash.hex}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+resource "aws_instance" "controller" {
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_types["controller"]}"
+  key_name               = "${aws_key_pair.auth.id}"
+  subnet_id              = "${aws_subnet.benchmark_subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.benchmark_security_group.id}"]
+  count                  = "${var.num_instances["controller"]}"
+
+  tags = {
+    Name      = "kafka_controller_${count.index}"
+    Benchmark = "Kafka"
+  }
+}
+
+resource "aws_instance" "broker" {
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_types["broker"]}"
+  key_name               = "${aws_key_pair.auth.id}"
+  subnet_id              = "${aws_subnet.benchmark_subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.benchmark_security_group.id}"]
+  count                  = lookup(var.num_instances, "broker", 0) # (var.num_instances["broker"]")
+
+  tags = {
+    Name      = "kafka_broker_${count.index}"
+    Benchmark = "Kafka"
+  }
+}
+
+resource "aws_instance" "client" {
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_types["client"]}"
+  key_name               = "${aws_key_pair.auth.id}"
+  subnet_id              = "${aws_subnet.benchmark_subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.benchmark_security_group.id}"]
+  count                  = "${var.num_instances["client"]}"
+
+  tags = {
+    Name      = "kafka_client_${count.index}"
+    Benchmark = "Kafka"
+  }
+}
+
+output "kafka_ssh_host" {
+  value = "${aws_instance.controller.0.public_ip}"
+}
+
+output "client_ssh_host" {
+  value = "${aws_instance.client.0.public_ip}"
+}
