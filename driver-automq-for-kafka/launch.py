0329cd752fdb07de8963968463477d7feaae78e7
Ning Yu <ningyu@automq.com>
2023-12-14 16:46:59 +0800

    style: format codes
    Signed-off-by: Ning Yu <ningyu@automq.com>

--- driver-automq-for-kafka/launch.py
+++ driver-automq-for-kafka/launch.py
@@ -12,17 +12,17 @@
 # limitations under the License.
 #
 
+import argparse
+import configparser
 import json
 import os
 import subprocess
-import configparser
-import argparse
 from pathlib import Path
 
 import boto3
 import yaml
 
-ami_prefix = 'dev-automq-kos-linux-aws-amd64'
+ami_prefix = "dev-automq-kos-linux-aws-amd64"
 
 
 def is_tool(name):
@@ -49,7 +49,9 @@ def get_afk_long_running_path():
 
 
 def extract_info_from_terraform_output():
-    tf_state_file = get_afk_long_running_path().joinpath("deploy").joinpath("terraform.tfstate")
+    tf_state_file = (
+        get_afk_long_running_path().joinpath("deploy").joinpath("terraform.tfstate")
+    )
     extracted_info = {}
     with open(tf_state_file) as f:
         data = json.load(f)
@@ -61,20 +63,35 @@ def extract_info_from_terraform_output():
 def create_clients():
     base_path = get_afk_long_running_path().joinpath("deploy")
     subprocess.run(args=["terraform", "init", "-upgrade"], check=True, cwd=base_path)
-    subprocess.run(args=["terraform", "apply", "-auto-approve"], check=True, cwd=base_path)
+    subprocess.run(
+        args=["terraform", "apply", "-auto-approve"], check=True, cwd=base_path
+    )
     return extract_info_from_terraform_output()
 
 
 def pkg_whole_project():
     base_path = get_afk_long_running_path().parent
-    subprocess.run(args=["mvn", "clean", "package", "-Dlicense.skip=true", "-Dcheckstyle.skip", "-DskipTests",
-                         "-Dspotless.check.skip", "-Dspotbugs.skip"],
-                   check=True, cwd=base_path)
+    subprocess.run(
+        args=[
+            "mvn",
+            "clean",
+            "package",
+            "-Dlicense.skip=true",
+            "-Dcheckstyle.skip",
+            "-DskipTests",
+            "-Dspotless.check.skip",
+            "-Dspotbugs.skip",
+        ],
+        check=True,
+        cwd=base_path,
+    )
 
 
 def modify_amq_install_yaml(file_path, dicts):
     # get tpl file path
-    tpl_file_path = get_afk_long_running_path().joinpath("amq-install").joinpath("deploy.yaml.tpl")
+    tpl_file_path = (
+        get_afk_long_running_path().joinpath("amq-install").joinpath("deploy.yaml.tpl")
+    )
     with open(tpl_file_path) as tpl:
         data = yaml.safe_load(tpl)
     for key, value in dicts.items():
@@ -84,7 +101,7 @@ def modify_amq_install_yaml(file_path, dicts):
 
 
 def update_nested_dict(data, key, value):
-    keys = key.split('.')
+    keys = key.split(".")
     current = data
 
     for k in keys[:-1]:
@@ -98,17 +115,23 @@ def update_nested_dict(data, key, value):
 def create_and_launch_servers(vpc_id):
     base_path = get_afk_long_running_path().joinpath("amq-install")
     ami_id = find_latest_ami()
-    ak = os.environ['AWS_ACCESS_KEY_ID']
-    sk = os.environ['AWS_SECRET_ACCESS_KEY']
+    ak = os.environ["AWS_ACCESS_KEY_ID"]
+    sk = os.environ["AWS_SECRET_ACCESS_KEY"]
     # update VPC ID in deploy.yaml
-    modify_amq_install_yaml(base_path.joinpath("deploy.yaml"), {
-        "vpcID": vpc_id,
-        "accessKey": ak,
-        "secretKey": sk,
-        "ec2.amiID": ami_id,
-    })
-    subprocess.run(args=["amq-installer", "install-kos", "-f", "deploy.yaml"],
-                   check=True, cwd=base_path)
+    modify_amq_install_yaml(
+        base_path.joinpath("deploy.yaml"),
+        {
+            "vpcID": vpc_id,
+            "accessKey": ak,
+            "secretKey": sk,
+            "ec2.amiID": ami_id,
+        },
+    )
+    subprocess.run(
+        args=["amq-installer", "install-kos", "-f", "deploy.yaml"],
+        check=True,
+        cwd=base_path,
+    )
 
     output_file = base_path.joinpath("privateEndpoint.txt")
     with open(output_file) as f:
@@ -119,13 +142,15 @@ def create_and_launch_servers(vpc_id):
 def change_servers_in_hosts_ini(file_path, bootstrap_servers):
     config = configparser.ConfigParser()
     config.read(file_path)
-    if not config.has_section('server'):
-        config.add_section('server')
+    if not config.has_section("server"):
+        config.add_section("server")
     # modify servers in hosts.ini
     for server in bootstrap_servers:
         private_ip = server.split(":")[0]
         # hosts.ini does not use the standard ini format, so we have to set values like this.
-        config['server']["%s ansible_user" % private_ip] = "ec2-user private_ip=%s" % private_ip
+        config["server"]["%s ansible_user" % private_ip] = (
+            "ec2-user private_ip=%s" % private_ip
+        )
     with open(file_path, "w") as f:
         # set space_around_delimiters=False to avoid adding spaces around the equal sign
         config.write(f, space_around_delimiters=False)
@@ -136,40 +161,54 @@ def launch_clients(bootstrap_servers):
     # modify servers in hosts.ini
     change_servers_in_hosts_ini(base_path.joinpath("hosts.ini"), bootstrap_servers)
     # deploy clients
-    subprocess.run(args=["ansible-playbook", "deploy.yaml", "-i", "hosts.ini", "--limit", "client"],
-                   check=True, cwd=base_path)
+    subprocess.run(
+        args=[
+            "ansible-playbook",
+            "deploy.yaml",
+            "-i",
+            "hosts.ini",
+            "--limit",
+            "client",
+        ],
+        check=True,
+        cwd=base_path,
+    )
 
 
 def find_latest_ami():
     """Find the latest AMI for development"""
-    ec2_client = boto3.client('ec2', region_name='cn-northwest-1')  # Change as appropriate
+    ec2_client = boto3.client(
+        "ec2", region_name="cn-northwest-1"
+    )  # Change as appropriate
     images = ec2_client.describe_images(
         Filters=[
             {
-                'Name': 'architecture',
-                'Values': [
-                    'x86_64',
-                ]
-            }, {
-                'Name': 'state',
-                'Values': [
-                    'available',
-                ]
-            }, {
-                'Name': 'tag:install',
-                'Values': [
-                    'AutoMQ Kafka',
-                ]
-            }
+                "Name": "architecture",
+                "Values": [
+                    "x86_64",
+                ],
+            },
+            {
+                "Name": "state",
+                "Values": [
+                    "available",
+                ],
+            },
+            {
+                "Name": "tag:install",
+                "Values": [
+                    "AutoMQ Kafka",
+                ],
+            },
         ],
-        Owners=['self']
+        Owners=["self"],
     )
-    images = images['Images']
+    images = images["Images"]
     # filter by name
-    images = [image for image in images if image['Name'].startswith(ami_prefix)]
+    images = [image for image in images if image["Name"].startswith(ami_prefix)]
     # sort by creation date in descending order
-    images = sorted(images, key=lambda image: image['CreationDate'], reverse=True)
-    return images[0]['ImageId']
+    images = sorted(images, key=lambda image: image["CreationDate"], reverse=True)
+    return images[0]["ImageId"]
 
 
 def launch_all():
@@ -189,13 +228,18 @@ def launch_all():
 
 def destroy_servers():
     base_path = get_afk_long_running_path().joinpath("amq-install")
-    subprocess.run(args=["amq-installer", "uninstall-kos", "-f", "deploy.yaml"],
-                   check=True, cwd=base_path)
+    subprocess.run(
+        args=["amq-installer", "uninstall-kos", "-f", "deploy.yaml"],
+        check=True,
+        cwd=base_path,
+    )
 
 
 def destroy_clients():
     base_path = get_afk_long_running_path().joinpath("deploy")
-    subprocess.run(args=["terraform", "destroy", "-auto-approve"], check=True, cwd=base_path)
+    subprocess.run(
+        args=["terraform", "destroy", "-auto-approve"], check=True, cwd=base_path
+    )
 
 
 def destroy_all():
@@ -206,10 +250,15 @@ def destroy_all():
     destroy_clients()
 
 
-if __name__ == '__main__':
-    if (os.environ.get('AWS_ACCESS_KEY_ID') is None) or (os.environ.get('AWS_SECRET_ACCESS_KEY') is None):
-        raise Exception("AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set in the environment")
-    parser = argparse.ArgumentParser(description='start or destroy the whole cluster')
+if __name__ == "__main__":
+    if (
+        os.environ.get("AWS_ACCESS_KEY_ID") is None
+        or os.environ.get("AWS_SECRET_ACCESS_KEY") is None
+    ):
+        raise Exception(
+            "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set in the environment"
+        )
+    parser = argparse.ArgumentParser(description="start or destroy the whole cluster")
     parser.add_argument("--up", help="launch all", action="store_true")
     parser.add_argument("--down", help="destroy all", action="store_true")
     args, leftovers = parser.parse_known_args()
